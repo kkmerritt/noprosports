b5f85c620387e89d017d4b2c67011aece54f2733
Kevin Merritt <kkmerritt@gmail.com>
2015-11-24 00:39:09 -0500

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :tasks
   resources :users
-  root 'tasks#index'
+  root 'application#welcome'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
