702c33df5607bf1bfa33193378265ebd38db6f13
Kevin Merritt <kkmerritt@gmail.com>
2015-11-24 15:48:48 -0500

    working, usering, modals

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,7 +5,12 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-
+  def remember(user)
+      user.remember
+      cookies.permanent.signed[:user_id] = user.id
+      cookies.permanent[:remember_token] = user.remember_token
+    end
+    
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
@@ -13,5 +18,11 @@ module SessionsHelper
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
+  end
+
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,24 @@
   class User < ActiveRecord::Base
+    attr_accessor :remember_token
+
     has_secure_password
 
 
     validates :username,  presence: true, uniqueness: true
     validates :password_digest, presence: true
     validates :password, presence: true
+
+
+    def User.digest(string)
+  cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                BCrypt::Engine.cost
+  BCrypt::Password.create(string, cost: cost)
+end
+
+
+    def remember
+      self.remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(remember_token))
+    end
 
   end
