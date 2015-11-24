a098d1806e872554acb5a2bee32f6a47d6ef0c00
Kevin Merritt <kkmerritt@gmail.com>
2015-11-24 18:26:02 -0500

    fixin heroku

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,8 +5,6 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-
-
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
@@ -15,7 +13,6 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
-
 
   def log_out
     session.delete(:user_id)
