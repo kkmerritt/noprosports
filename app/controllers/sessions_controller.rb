117e30630a3a651b9cc1c91ffd388148ec28750a
Kevin Merritt <kkmerritt@gmail.com>
2015-11-24 17:26:55 -0500

    working alright

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,6 @@ class SessionsController < ApplicationController
       user = User.find_by(username: params[:session][:username].downcase)
       if user && user.authenticate(params[:session][:password])
         log_in user
-        remember user
         redirect_to user
       else
         flash[:danger] = 'Invalid email/password combination'
